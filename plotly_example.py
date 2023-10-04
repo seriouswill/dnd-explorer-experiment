2f90e915cde0971ad066feb456a2f61bb7fda811
Will O'Connell <williamjamesoc@gmail.com>
2023-10-04 16:17:31 +0100

    plotly interactive on ec2 test

--- plotly_example.py
+++ plotly_example.py
@@ -1,48 +1,54 @@
 from confluent_kafka import Consumer, KafkaError
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 import plotly.graph_objs as go
 import json
+from dash.dependencies import Output, Input
 
+# Kafka Configuration
 conf = {
-    'bootstrap.servers': 'b-1.monstercluster1.6xql65.c3.kafka.eu-west-2.amazonaws.com:9092',  # Replace with your broker URLs
+    'bootstrap.servers': 'b-1.monstercluster1.6xql65.c3.kafka.eu-west-2.amazonaws.com:9092',
     'group.id': 'your_group_id',
     'auto.offset.reset': 'earliest',
 }
 
 consumer = Consumer(conf)
+consumer.subscribe(['monster-damage'])
 
-# List to collect structured messages
-messages_list = []
+# Dash Configuration
+app = dash.Dash(__name__, external_stylesheets=["https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"])
 
-def consume_message():
-    consumer.subscribe(['monster-damage'])
-    while True:
+app.layout = html.Div([
+    dcc.Graph(id='live-graph', animate=True),
+    dcc.Interval(
+        id='graph-update',
+        interval=1*1000,  # in milliseconds
+        n_intervals=0
+    ),
+])
+
+@app.callback(Output('live-graph', 'figure'),
+              [Input('graph-update', 'n_intervals')])
+def update_graph(n):
+    global consumer
+
+    messages_list = []
+    for _ in range(10):  # Collect 10 messages
         msg = consumer.poll(1.0)
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            else:
-                print(msg.error())
-                break
+        if msg is not None and not msg.error():
+            structured_msg = json.loads(msg.value().decode('utf-8'))
+            messages_list.append(structured_msg)
 
-        # Convert message from JSON string to dictionary
-        structured_msg = json.loads(msg.value().decode('utf-8'))
-        messages_list.append(structured_msg)
-
-        # Display updated visualization every 10 messages
-        if len(messages_list) % 10 == 0:
-            visualize_data()
-
-def visualize_data():
     countries = [entry['country'] for entry in messages_list]
     percent_losses = [entry['percent_loss'] for entry in messages_list]
 
     fig = go.Figure(data=[go.Bar(x=countries, y=percent_losses)])
-    fig.update_layout(title='Percent Population Loss by Country', 
+    fig.update_layout(title='Percent Population Loss by Country',
                       xaxis=dict(title='Country'),
                       yaxis=dict(title='Percent Loss'))
-    fig.show()
+    
+    return fig
 
-consume_message()
-consumer.close()
+if __name__ == '__main__':
+    app.run_server(debug=True)

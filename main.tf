d08abb35111e0a5fac56b2c794464eb778972cf3
Will O'Connell <williamjamesoc@gmail.com>
2023-10-19 09:58:45 +0100

    added MSK steps to terraform script

--- main.tf
+++ main.tf
@@ -16,6 +16,49 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+# Existing variables and configurations ...
+
+resource "aws_msk_cluster" "monster_cluster" {
+  cluster_name           = "monster-cluster-1"
+  kafka_version          = "2.8.1"
+  number_of_broker_nodes = 3
+  
+  broker_node_group_info {
+    instance_type   = "kafka.t3.small"
+    ebs_volume_size = 100
+    client_subnets = [
+      "subnet-a41657de", 
+      "subnet-7213161b", 
+      "subnet-3c118570"
+    ]
+    security_groups = ["sg-39d6e247"]
+  }
+
+  encryption_info {
+    encryption_at_rest_kms_key_arn = "arn:aws:kms:eu-west-2:404544469985:key/33a39a70-daa6-4f13-a5d1-6e198a96cd4c"
+    encryption_in_transit {
+      client_broker = "PLAINTEXT"
+      in_cluster    = true
+    }
+  }
+
+  client_authentication {
+    unauthenticated = true
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = false
+      }
+      node_exporter {
+        enabled_in_broker = false
+      }
+    }
+  }
+}
+
+
 resource "aws_instance" "msk_client" {
   ami             = "ami-0b2287cff5d6be10f" # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
   instance_type   = "t2.small"
@@ -40,7 +83,7 @@ resource "aws_instance" "msk_client" {
 
 
   tags = {
-    Name = "WillsMonsterEC2"
+    Name = "MakersMonsterEC2"
   }
 
     
